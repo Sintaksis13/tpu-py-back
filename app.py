935843fe9aa29132ef01460d5fc05911a0745e44
Sintaksis13 <jandos13331@gmail.com>
2020-02-27 17:03:29 +0700

    fix

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ class AdminCheck(Resource):
 
 def replaceId(self, product):
     return {'id': str(product.get('_id')), 'title': product.get('title'), 
-    'description': product.get('description'), 'price': product.get('price'))}
+    'description': product.get('description'), 'price': product.get('price')}
 
 
 api.add_resource(Red, '/color/red')
