138d91c3101bacc0bb68ae259c8c4589fca8ddc6
abdullin_rr <abdullin_rr@bw-sw.com>
2020-02-28 11:24:31 +0700

    Final improvements

--- app.py
+++ app.py
@@ -4,18 +4,21 @@ from flask_cors import CORS, cross_origin
 from pymongo import MongoClient
 from dataclasses import dataclass
 from bson.objectid import ObjectId
+from typing import List
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 CORS(app)
 api = Api(app)
-client = MongoClient('mongodb://db:27017')
+client = MongoClient('mongodb://localhost:27017')
 db = client.projectDB
 
 #collections
 productsCollection = db['product']
+ordersCollection = db['order']
 
 
+#First lab
 class Red(Resource):
     def get(self):
         return jsonify({'color': 'red'})
@@ -36,6 +39,8 @@ class White(Resource):
     def get(self):
         return jsonify({'color': 'white'})
 
+#Second lab
+#Product
 class GetAllProducts(Resource):
     def get(self):
         products = productsCollection.find()
@@ -44,7 +49,7 @@ class GetAllProducts(Resource):
 
     def _from_document_to_product(self, product):
         return Product(id=str(product.get('_id')), title=product.get('title'), 
-        description=product.get('description'), price=product.get('price'))
+            description=product.get('description'), price=product.get('price'))
 
 class CreateProduct(Resource):
     def post(self):
@@ -56,6 +61,50 @@ class DeleteProduct(Resource):
     def delete(self, id):
         productsCollection.delete_one({ "_id": ObjectId(id) })
 
+#Order
+class CreateOrder(Resource):
+    def post(self):
+        data = request.get_json()
+        inserted = ordersCollection.insert_one(data)
+        id = str(inserted.inserted_id)
+        print(id)
+        return id
+
+class GetAllOrders(Resource):
+    def get(self):
+        orders = ordersCollection.find()
+        order_list = list(map(self._from_document_to_order, orders))
+        return jsonify(order_list)
+
+    def _from_document_to_order(self, order):
+        return Order(id=str(order.get('_id')), orderLines=order.get('orderLines'), 
+            totalAmount=order.get('totalAmount'), customerData=order.get('customerData'))
+
+class GetOrder(Resource):
+    def get(self, id):
+        if "\"" in id:
+            id = id[1:-1] 
+        order = ordersCollection.find_one({ "_id": ObjectId(id) })
+        print(order)
+        orderLines = order.get('orderLines')
+        print(orderLines)
+        richOrderLines = list(map(self._enrichOrderLines, orderLines))
+        print(richOrderLines)
+        richOrder = RichOrder(id=str(order.get('_id')), orderLines=richOrderLines, 
+            totalAmount=order.get('totalAmount'), customerData=order.get('customerData'))
+        return jsonify(richOrder)
+
+    def _enrichOrderLines(self, orderLine):
+        product = productsCollection.find_one({ "_id": ObjectId(orderLine.get('productId')) })
+        entity = self._from_document_to_product(product)
+        return RichOrderLine(product=entity, count=orderLine.get('count'))
+
+    def _from_document_to_product(self, product):
+        return Product(id=str(product.get('_id')), title=product.get('title'), 
+            description=product.get('description'), price=product.get('price'))
+
+
+#AdminCheck
 class AdminCheck(Resource):
     def post(self):
         adminPass = 'pass'
@@ -76,15 +125,52 @@ class Product:
     description: str
     price: float
 
+@dataclass(frozen=True)
+class OrderLine:
+    productId: str
+    count: int
 
+@dataclass(frozen=True)
+class CustomerData:
+    name: str
+    address: str
+    phone: str
+
+@dataclass(frozen=True)
+class Order:
+    id: str
+    orderLines: List[OrderLine]
+    totalAmount: float
+    customerData: CustomerData
+
+@dataclass(frozen=True)
+class RichOrderLine:
+    product: Product
+    count: int
+
+@dataclass(frozen=True)
+class RichOrder:
+    id: str
+    orderLines: List[RichOrderLine]
+    totalAmount: float
+    customerData: CustomerData
+
+
+#urls mapping
 api.add_resource(Red, '/color/red')
 api.add_resource(Blue, '/color/blue')
 api.add_resource(Green, '/color/green')
 api.add_resource(Yellow, '/color/yellow')
 api.add_resource(White, '/color/')
+
 api.add_resource(GetAllProducts, '/products')
 api.add_resource(CreateProduct, '/products')
 api.add_resource(DeleteProduct, '/products/<id>')
+
+api.add_resource(CreateOrder, '/orders')
+api.add_resource(GetAllOrders, '/orders')
+api.add_resource(GetOrder, '/orders/<id>')
+
 api.add_resource(AdminCheck, '/admin/check')
 
 
